c07ccbb0f583537bd0b43ebc5b33f85311373bae
Bianca Csan <32331590+ebiacsan@users.noreply.github.com>
2019-05-01 17:51:32 -0300

    JdF
    Hearders file do jogo

---
+++ Jogo_da_Forca.h
@@ -0,0 +1,12 @@
+#define TAM_PALAVRA 20
+
+void cabecalho();
+void captura_palpites();
+int letraJaChutada(char letrachutada);
+void desenhaForca();
+void escolhePalavra();
+int acertou();
+int morreu();
+void adicionaPalavra();
+int letraExiste(char letraTotal);
+int chutesErrados();
